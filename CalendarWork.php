86fd24beeeb2d2ee2237fd391ea5989dde6b017e
John 'Doc' Hayward <John.Hayward@wheaton.edu>
2014-09-11 13:43:37 +0000

    new file:   CalendarWork.php Added CalendarWork.php which shows how to use Calendar on web page

---
+++ CalendarWork.php
@@ -0,0 +1,5 @@
+<a href=https://www.google.com/calendar/embed?src=m6sr0ll4k69k2srdju1trquip0%40group.calendar.google.com&ctz=America/Chicago > Calendar</a>
+<div style="overflow: hidden; width: 85%; height: 90%;">
+<p style="width: 140%; height: 105%;">
+<iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=m6sr0ll4k69k2srdju1trquip0%40group.calendar.google.com&amp;color=%236B3304&amp;ctz=America%2FChicago&amp;dates=20141001%2F20141030" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+</p></div>
